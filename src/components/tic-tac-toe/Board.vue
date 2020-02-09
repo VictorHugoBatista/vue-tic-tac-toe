110289e1315ef0ad2ae10b14ad48676b69392f90
Victor Hugo <vct.bta@gmail.com>
2020-02-09 10:10:27 -0300

    Map the boardbuttons by the state

--- src/components/tic-tac-toe/Board.vue
+++ src/components/tic-tac-toe/Board.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <BoardButton />
+    <b-row v-for="(row, rowNumber) in this.$store.state.board" v-bind:key="rowNumber">
+      <b-col v-for="(button, buttonNumber) in row" v-bind:key="buttonNumber">
+        <BoardButton :selection="button" />
+      </b-col>
+    </b-row>
   </div>
 </template>
 
