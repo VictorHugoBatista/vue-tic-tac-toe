c29a05b0cca5d9ec589f144a5b535712a0aff19f
Victor Hugo Batista <victor.batista@i9xp.com.br>
2020-02-10 15:32:51 -0300

    Show the current player on the screen

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,10 @@
     <div class="container">
       <h1 class="text-center">Tic tac toe in Vue</h1>
       <Board />
+      <div class="mt-5 text-center">
+        <h2 class="mb-0">Current player</h2>
+        <BoardButton :selection="this.$store.state.turn" />
+      </div>
       <div class="text-center mt-5">
         <github-button class="mr-3" href="https://github.com/VictorHugoBatista">Follow</github-button>
         <github-button data-icon="star" href="https://github.com/VictorHugoBatista/vue-tic-tac-toe">Star</github-button>
@@ -14,12 +18,14 @@
 
 <script>
 import Board from './components/tic-tac-toe/Board'
+import BoardButton from './components/tic-tac-toe/BoardButton'
 import GithubButton from 'vue-github-button'
 
 export default {
   name: 'App',
   components: {
     Board,
+    BoardButton,
     GithubButton,
   },
 }
